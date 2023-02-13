974f9218311e3c532d7c35eb3fdcf77a941f131c
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-02-13 14:59:54 +0900

    fix: add overflow hidden option

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Sidebar from "./components/Sidebar";
 
 function Main() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex h-full w-full">
+      <Sidebar />
+      <div className="flex justify-center	items-center grow">
+        <div className="text-5xl">Chart</div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
