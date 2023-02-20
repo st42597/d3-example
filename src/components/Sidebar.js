c1f09226caf70bde1888032431b27d9668f5c09f
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-02-20 16:59:57 +0900

    refactor: refactor sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,23 +10,25 @@ const sidebarOption = [
 
 function Sidebar() {
   return (
-    <nav className="h-full w-80 bg-emerald-300 p-6 overflow-y-auto">
+    <nav className="h-full w-80 bg-emerald-300 p-6">
       <h1 className="text-2xl flex justify-center items-center font-bold mb-6 cursor-pointer">
         D3.js-example
       </h1>
-      {sidebarOption.map((item) => (
-        <NavLink
-          to={item.path}
-          className={({ isActive }) => {
-            const defaultStyle =
-              "text-xl flex justify-center items-center my-2 p-2 cursor-pointer hover:bg-slate-400/[.4]";
-            return isActive ? defaultStyle + " font-bold" : defaultStyle;
-          }}
-          key={item.name}
-        >
-          {item.name}
-        </NavLink>
-      ))}
+      <div className="overflow-y-auto">
+        {sidebarOption.map((item) => (
+          <NavLink
+            to={item.path}
+            className={({ isActive }) => {
+              const defaultStyle =
+                "text-xl flex justify-center items-center my-2 p-2 cursor-pointer hover:bg-slate-400/[.4]";
+              return isActive ? defaultStyle + " font-bold" : defaultStyle;
+            }}
+            key={item.name}
+          >
+            {item.name}
+          </NavLink>
+        ))}
+      </div>
     </nav>
   );
 }
